ffa857a6ae0e4784270b490a61fa8dbf2b13451b
MAK <40379551+Mak1003@users.noreply.github.com>
2025-09-16 22:00:50 +0530

    commit

--- Practical_3/src/main/java/com/devops/jenkins/Application.java
+++ Practical_3/src/main/java/com/devops/jenkins/Application.java
@@ -1,4 +1,4 @@
-package com.devops.jenkins;
+package main.java.com.devops.jenkins;
 
 import java.util.List;
 import java.util.Scanner;
--- Practical_3/src/main/java/com/devops/jenkins/Calculator.java
+++ Practical_3/src/main/java/com/devops/jenkins/Calculator.java
@@ -1,4 +1,4 @@
-package com.devops.jenkins;
+package main.java.com.devops.jenkins;
 
 public class Calculator {
 
